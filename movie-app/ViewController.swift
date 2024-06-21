3c70d65b37246610bd4e0c8d0ce6387d2253c9af
Atakan Şenoğlu <atakansenoglu3@gmail.com>
2024-06-21 23:30:44 +0300

    Initial Commit

---
+++ movie-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-app
+//
+//  Created by Atakan Şenoğlu on 21/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
